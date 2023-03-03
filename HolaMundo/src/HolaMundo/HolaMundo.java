45583898da951e286ef31c1ef5faa6bfdefff524
marco <marco@SistemasUbuntu>
2023-03-03 14:52:50 +0100

    Commit 3

--- HolaMundo/src/HolaMundo/HolaMundo.java
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -3,7 +3,7 @@ package HolaMundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// Esto es una prueba
+		// Esto es una prueba de git	
 		System.out.println("Hola Mundo");
 	}
 
