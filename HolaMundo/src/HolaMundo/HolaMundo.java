4cdf2c7a9dd5b78bdc3321e9ab6b29e265208b35
marco <marco@SistemasUbuntu>
2023-03-03 14:54:59 +0100

    Prueba

--- HolaMundo/src/HolaMundo/HolaMundo.java
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -3,7 +3,7 @@ package HolaMundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// Esto es una prueba de git	
+		// Esto es una prueba de git	dfsdfsdfsd
 		System.out.println("Hola Mundo");
 	}
 
